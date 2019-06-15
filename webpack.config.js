c01aa63a7a157f7ab5afa9802d44b401943e5451
sandtr0ut <crosbyverse@gmail.com>
2019-06-14 19:12:33 -0700

    customize sass and run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true,
-                        // options...
-                    }
-                }
-            ]
-        }]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].bundle.css'
-        }),
-    ]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          'sass-loader'
+        ]
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/mystyles.css'),
+  ]
 };
